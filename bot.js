fa89fd5580fa27be226e5ab26689f0052d9cf366
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-12-08 21:22:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivite"&help",{type: 'STREAMING'})
+   client.user.setActivity"&help",{type: 'STREAMING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
